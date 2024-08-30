e46015c4bb89b6ee5f8039910ded8486ed3bc42d
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2024-08-30 13:10:04 -0300

    primeiro commit: subindo projeto

---
+++ dist/js/models/negociacoes.js
@@ -0,0 +1,13 @@
+export class Negociacoes {
+    constructor() {
+        this.negociacoes = [];
+    }
+    adiciona(negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+    lista() {
+        return this.negociacoes;
+    }
+}
+//instanciando ou seja, criando uma lista vazia
+const negociacoes = new Negociacoes();
